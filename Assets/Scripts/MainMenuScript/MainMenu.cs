ea463dbca85e2151e60a03b1168ca57887abd674
Emma Tethrain <emmateth@psq.local>
2023-09-03 16:16:09 +0200

    Menu

--- Assets/Scripts/MainMenuScript/MainMenu.cs
+++ Assets/Scripts/MainMenuScript/MainMenu.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class SC_MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    { 
-        SceneManager.LoadScene("In-gameScreen")
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
     }
 
+    public void PlayButton()
+    {
+        // Play Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameLevel");
+    }
+
+    public void CreditsButton()
+    {
+        // Show Credits Menu
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+    }
 }
