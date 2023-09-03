c839a1ed6e6386244b0d58085cd71845fa7bdf87
Oskar Nord <oskanord@psq.local>
2023-09-03 13:18:16 +0200

    updated critterpickup and trashcan

--- Assets/Scripts/TrashCan.cs
+++ Assets/Scripts/TrashCan.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 public class TrashCan : MonoBehaviour
 {
     public float trashRange = 5.0f;
+    public int score = 0;
 
     private void Update()
     {
         Collider[] colliders = Physics.OverlapSphere(transform.position, trashRange);
-
-
         foreach (Collider collider in colliders)
         {
             if (collider.CompareTag("Critter"))
             {
                 GameObject critter = collider.gameObject;
                 critter.SetActive(false);
+                score += 10;
             }
         }
     }
